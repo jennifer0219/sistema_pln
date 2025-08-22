e4f02db38833d4423215e4e3a43ccb2c572f729e
jennifer0219 <galiciajennifer227@gmail.com>
2025-08-21 20:08:55 -0600

    Agregue funcionalidad de histograma y modifique views con urls

--- analisis/urls.py
+++ analisis/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('subir/', views.subir_texto, name='subir_texto'),
     path('', views.lista_textos, name='lista_textos'),
+    path('histograma/<int:texto_id>/', views.histograma, name='histograma'),
 ]
--- analisis/views.py
+++ analisis/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TextoAnalizadoForm
 from .models import TextoAnalizado
+from collections import Counter
+import re
 
 def subir_texto(request):
     if request.method == 'POST':
@@ -15,3 +17,17 @@ def subir_texto(request):
 def lista_textos(request):
     textos = TextoAnalizado.objects.all().order_by('-fecha_subida')
     return render(request, 'analisis/lista.html', {'textos': textos})
+
+def histograma(request, texto_id):
+    texto = get_object_or_404(TextoAnalizado, id=texto_id)
+    with open(texto.archivo.path, "r", encoding="utf-8") as f:
+        contenido = f.read().lower()
+
+    # separar palabras con regex
+    palabras = re.findall(r'\w+', contenido)
+    contador = Counter(palabras)
+
+    return render(request, 'analisis/histograma.html', {
+        'texto': texto,
+        'contador': contador.most_common()  # lista de (palabra, frecuencia)
+    })
